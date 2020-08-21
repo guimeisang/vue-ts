71f5f5f8eacecfd4641c473ec67cf366679402a9
GM <zhangguimei@cvte.com>
2020-08-22 01:06:48 +0800

    [modify]修改代码

---
+++ src/components/Project.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="project">1123456789000</div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+export default class Project extends Vue {}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HelloWorld
+      msg="Welcome to Your Vue.js + TypeScript App"
+      name="GM"
+      age="18"
+      address="GZ"
+      job="IT"
+    />
   </div>
 </template>
 
